88d230ff91879fd130ab38dcac732f1ba12f38b3
kuomory <komori.kumori@gmail.com>
2024-11-13 06:17:10 +0000

    Save Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,134 @@
 import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
+import {
+  AppShell,
+  Button,
+  Container,
+  Group,
+  MantineProvider,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
 import { theme } from "./theme";
+import { useCallback, useState } from "react";
+import { IconDownload } from "@tabler/icons-react";
+
+// type ShowSaveFilePickerType = (options?: {
+//   suggestedName?: string;
+//   types?: Array<{
+//     description?: string;
+//     accept?: Record<string, string[]>;
+//   }>;
+//   excludeAcceptAllOption?: boolean;
+// }) => Promise<FileSystemFileHandle>;
+// type ShowOpenFilePickerType = (options?: {
+//   multiple?: boolean;
+//   excludeAcceptAllOption?: boolean;
+//   types?: Array<{
+//     description?: string;
+//     accept?: Record<string, string[]>;
+//   }>;
+// }) => Promise<FileSystemFileHandle[]>;
+
+// type WindowWithShowSaveFilePicker = Window &
+//   typeof globalThis & {
+//     showSaveFilePicker?: ShowSaveFilePickerType;
+//     showOpenFilePicker?: ShowOpenFilePickerType;
+//   };
 
 export default function App() {
-  return <MantineProvider theme={theme}>App</MantineProvider>;
+  const [fileName, setFileName] = useState(
+    (window as any).myStore?.fileName ?? ""
+  );
+  const handleSave = useCallback(async () => {
+    const myStore = { fileName: fileName };
+    const htmlContent = document.documentElement.outerHTML;
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlContent, "text/html");
+    doc.body.innerHTML = `<div id="root"></div><script>window.myStore = ${JSON.stringify(
+      myStore
+    )}</script>`;
+    const updatedHtmlContent = `<!DOCTYPE html>\n${doc.documentElement.outerHTML}`;
+
+    const blob = new Blob([updatedHtmlContent], { type: "text/html" });
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = `${fileName || "新しいプレゼンテーション"}.pbp.html`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(link.href);
+  }, [fileName]);
+  return (
+    <MantineProvider theme={theme}>
+      <AppShell header={{ height: "4rem" }}>
+        <AppShell.Header>
+          <Group h="100%" px="md" justify="space-between">
+            <Text
+              variant="gradient"
+              gradient={{ from: "indigo", to: "grape", deg: 45 }}
+              size="1rem"
+              lh="0.9rem"
+              fw="normal"
+            >
+              <span
+                style={{
+                  fontWeight: "bold",
+                  fontStyle: "normal",
+                  fontSize: "1.1rem",
+                }}
+              >
+                P
+              </span>
+              ortable
+              <br />
+              <span
+                style={{
+                  fontWeight: "bold",
+                  fontStyle: "normal",
+                  fontSize: "1.1rem",
+                }}
+              >
+                B
+              </span>
+              rowser
+              <br />
+              <span
+                style={{
+                  fontWeight: "bold",
+                  fontStyle: "normal",
+                  fontSize: "1.1rem",
+                }}
+              >
+                P
+              </span>
+              resenter
+            </Text>
+            <Group>
+              <TextInput
+                value={fileName}
+                onChange={(e) => {
+                  setFileName(e.target.value);
+                }}
+                placeholder="新しいプレゼンテーション"
+                w="20rem"
+              />
+              <Button
+                onClick={handleSave}
+                leftSection={<IconDownload />}
+                variant="rich"
+              >
+                ダウンロード
+              </Button>
+            </Group>
+          </Group>
+        </AppShell.Header>
+        <AppShell.Main>
+          <Container>
+            <Stack>Main</Stack>
+          </Container>
+        </AppShell.Main>
+      </AppShell>
+    </MantineProvider>
+  );
 }
